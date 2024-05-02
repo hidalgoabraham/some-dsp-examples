aa39fd397cc5d9312abbe16bb76fc56cf0034430
hidalgoabraham <ahidalgo3@uc.edu.ve>
2024-05-02 07:49:02 -0400

    update

--- example-1.5/3d-plot.py
+++ example-1.5/3d-plot.py
@@ -26,28 +26,6 @@ Xz_abs = np.clip(Xz_abs, None, 5)
 surface = go.Surface(x=re_z, y=im_z, z=Xz_abs, colorscale="Turbo", showscale=False)
 fig.add_trace(surface, row=1, col=1)
 
-
-# Xz_r1 = np.zeros(shape=[len(x_grid), len(x_grid[0])], dtype="complex_")
-
-# for i in range(len(re_z)):
-#     for j in range(len(im_z)):
-#         x = re_z[i]
-#         y = im_z[j]
-#         r = np.sqrt(x**2 + y**2)
-
-#         if round(r, 2) == 1:
-#             c = x + 1j * y
-#             w = np.angle(c)
-
-#             zeta = round(r, 2) * np.e ** (1j * w)
-#             Xz_r1[i][j] = zeta**3 + zeta**2 + zeta**1 + zeta**0 + zeta**-1 + zeta**-2 + zeta**-3
-
-#         else:
-#             Xz_r1[i][j] = 0
-
-# surface = go.Surface(x=re_z, y=im_z, z=np.absolute(Xz_r1), colorscale="Turbo", showscale=False)
-# fig.add_trace(surface, row=1, col=2)
-
 fig.update_layout(title_text="Z Transform", height=800, width=1600)
 
 fig.update_layout(
--- example-1.6/calculations.py
+++ example-1.6/calculations.py
@@ -1,27 +1,3 @@
-# import sympy as sp
-
-# r, n, wo, z = sp.symbols("r, n, wo, z")
-
-# # r = sp.symbols("r", type=sp.Integer, real=True)
-# # n = sp.symbols("n", type=sp.Integer, real=True)
-# # wo = sp.symbols("wo", type=sp.Float, real=True, positive=True)
-# # z = sp.symbols("z", type=sp.ComplexField, complex=True)
-
-# # expr1 = (r * sp.E ** (sp.I * wo) * z**-1) ** n
-# # expr2 = (r * sp.E ** (-sp.I * wo) * z**-1) ** n
-# # S1 = sp.Sum(expr1, (n, 0, sp.oo)).doit()
-# # S2 = sp.Sum(expr2, (n, 0, sp.oo)).doit()
-
-# # Xz = S1 + S2
-
-# expr = sp.cos(wo * n) * z**-n
-
-# Xz = sp.Sum(expr, (n, 0, sp.oo)).doit()
-
-# response =
-
-# sp.pprint(response)
-
 import lcapy as lp
 import sympy as sp
 
