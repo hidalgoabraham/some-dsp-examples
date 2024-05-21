7f6a82fd6bbd3cf9de29a2b462e134bda880c753
hidalgoabraham <ahidalgo3@uc.edu.ve>
2024-05-21 08:07:50 -0400

    update

--- Unit-2/example-2.3/plots.py
+++ Unit-2/example-2.3/plots.py
@@ -30,41 +30,51 @@ def sample_this_signal(signal, t, T):
 t = np.linspace(ti, tf, 201)
 xc_t = 0.01 * (t / 10 - 10) * ((t / 10 - 10) - 10) * ((t / 10 - 10) + 10) * ((t / 10 - 10) + 5) + 20
 
-Ta = 10
-xsa_t, na = sample_this_signal(xc_t, t, Ta)
+T = 20
+xs_t, n = sample_this_signal(xc_t, t, T)
+x_n = xs_t
 
-Tb = 2 * Ta
-xsb_t, nb = sample_this_signal(xc_t, t, Tb)
+xr_t = np.zeros(len(t))
 
-xa_n = xsa_t
-xb_n = xsb_t
+signals = []
+for i in range(len(n)):
+    if not n[i] is None:
+        signal = x_n[i] * np.sin(np.pi * (t - n[i] * T) / T) / (np.pi * (t - n[i] * T) / T)
+
+        # Correct invalid values
+        for index, value in enumerate(signal):
+            if t[index] - n[i] * T == 0:
+                signal[index] = x_n[i]
+
+        signals.append(signal)
+        xr_t += signal
 
 
 fig, axs = plt.subplots(2, 2)
 
-axs[0, 0].set_title(f"T = {Ta} (s)")
+axs[0, 0].set_title(f"")
 axs[0, 0].plot(t, xc_t, "b", label="Xc(t)")
-axs[0, 0].plot(t, xsa_t, ".r", label="Xs(t)")
 axs[0, 0].set_xlabel("t (s)")
 axs[0, 0].grid(True)
 axs[0, 0].legend()
 
-axs[0, 1].set_title(f"T = {Tb} (s)")
-axs[0, 1].plot(t, xc_t, "b", label="Xc(t)")
-axs[0, 1].plot(t, xsb_t, ".r", label="Xs(t)")
+axs[0, 1].set_title(f"T = {T} (s)")
+axs[0, 1].plot(t, xs_t, ".r", label="Xs(t)")
 axs[0, 1].set_xlabel("t (s)")
 axs[0, 1].grid(True)
 axs[0, 1].legend()
 
-axs[1, 0].set_title(f"T = {Ta} (s)")
-axs[1, 0].plot(na, xa_n, ".r", label="X[n]")
-axs[1, 0].set_xlabel("n")
+axs[1, 0].set_title(f"")
+for signal in signals:
+    axs[1, 0].plot(t, signal, "", label="")
+axs[1, 0].plot(t, xs_t, ".r", label="Xs(t)")
+axs[1, 0].set_xlabel("t")
 axs[1, 0].grid(True)
 axs[1, 0].legend()
 
-axs[1, 1].set_title(f"T = {Tb} (s)")
-axs[1, 1].plot(nb, xb_n, ".r", label="X[n]")
-axs[1, 1].set_xlabel("n")
+axs[1, 1].set_title(f"")
+axs[1, 1].plot(t, xr_t, "", label="Xr(t)")
+axs[1, 1].set_xlabel("t")
 axs[1, 1].grid(True)
 axs[1, 1].legend()
 
